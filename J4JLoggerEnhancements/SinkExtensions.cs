276bbd8f52f5f2125dad64186e7feba9253bcd79
Mark Olbert <mark@arcabama.com>
2023-02-27 11:07:17 -0800

    implement sinks

---
+++ J4JLoggerEnhancements/SinkExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Events;
+
+namespace J4JSoftware.Logging;
+
+public static class SinkExtensions
+{
+    public static LoggerConfiguration LastEvent(this LoggerSinkConfiguration loggerConfig,
+        out LastEventSink sink,
+        LogEventLevel restrictedToMinimumLevel = LogEventLevel.Verbose)
+    {
+        sink = new LastEventSink();
+
+        return loggerConfig.Sink(sink, restrictedToMinimumLevel);
+    }
+
+    public static LoggerConfiguration NetEvent(this LoggerSinkConfiguration loggerConfig,
+        string outputTemplate = NetEventSink.DefaultTemplate,
+        LogEventLevel restrictedToMinimumLevel = LogEventLevel.Verbose) =>
+        loggerConfig.Sink(new NetEventSink(outputTemplate), restrictedToMinimumLevel);
+
+    public static ILogger SourceCode(this ILogger logger,
+        [CallerMemberName] string memberName = "",
+        [CallerFilePath] string sourceFilePath = "",
+        [CallerLineNumber] int sourceLineNumber = 0)
+    {
+        return logger
+            .ForContext("MemberName", memberName)
+            .ForContext("FilePath", TemplateExtensions.TrimPath(sourceFilePath))
+            .ForContext("LineNumber", sourceLineNumber);
+    }
+
+    public static ILogger SendToSms(this ILogger logger)
+    {
+        return logger.ForContext("SendToSms", true);
+    }
+}
