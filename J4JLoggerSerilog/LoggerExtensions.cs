36e5d0d26d4c4f418364a89e6e7786ae32931011
Mark Olbert <mark@arcabama.com>
2023-03-16 11:46:48 -0700

    based path trimming on enricher

--- J4JLoggerSerilog/LoggerExtensions.cs
+++ J4JLoggerSerilog/LoggerExtensions.cs
@@ -1,13 +1,10 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using Serilog;
 
 namespace J4JSoftware.Logging;
 
 public static class LoggerExtensions
 {
-    private static readonly Dictionary<string, string?> AssemblySourceRoots = new();
-
     public static ILogger SourceCode(
         this ILogger logger,
         bool include = true,
@@ -18,24 +15,10 @@ public static class LoggerExtensions
         if (!include)
             return logger;
 
-        var srcRoot = GetSourceRoot(callerName);
-        callerName = srcRoot == null ? callerName : callerName.Replace(srcRoot, string.Empty);
-
         return logger
             .ForContext(LoggerTerms.CallerMemberElementName, callerName)
             .ForContext(LoggerTerms.CallerPathElementName, sourceFilePath)
             .ForContext(LoggerTerms.CallerLineNumElementName, sourceLineNumber);
-    }
-
-    private static string? GetSourceRoot(string callerName)
-    {
-        if( AssemblySourceRoots.ContainsKey(callerName))
-            return AssemblySourceRoots[callerName];
-
-        var attr = Assembly.GetExecutingAssembly().GetCustomAttribute<SourceCodeRootPathAttribute>();
-        AssemblySourceRoots.Add(callerName, attr?.RootPath);
-
-        return attr?.RootPath;
     }
 
     public static ILogger SendToSms(this ILogger logger, bool send = true) =>
